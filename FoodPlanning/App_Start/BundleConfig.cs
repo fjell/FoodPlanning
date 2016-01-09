8025ea5690c81e7f4630bbee1ad48e8251620bab
fjell <dia_peter@hotmail.com>
2016-01-09 16:32:06 +0100

    Added bundling

---
+++ FoodPlanning/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace FoodPlanning
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Framework CSS
+
+            // Self defined CSS
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/Content/Styles/Styles.css"));
+
+            // Framework JS
+
+            //Self defined JS
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/Script.js"));
+        }
+    }
+}
