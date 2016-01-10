277733bdf94da4df2754b663e948a14c86b16ace
fjell <dia_peter@hotmail.com>
2016-01-10 01:39:22 +0100

    added boostrap 4

--- FoodPlanning/App_Start/BundleConfig.cs
+++ FoodPlanning/App_Start/BundleConfig.cs
@@ -7,12 +7,16 @@ namespace FoodPlanning
         public static void RegisterBundles(BundleCollection bundles)
         {
             // Framework CSS
+            bundles.Add(new StyleBundle("~/bundles/frameworkCss").Include(
+                        "~/Content/Styles/Framework/Bootstrap/bootstrap.css"));
 
             // Self defined CSS
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                         "~/Content/Styles/Style.css"));
 
             // Framework JS
+            bundles.Add(new ScriptBundle("~/bundles/frameworkJs").Include(
+                        "~/Scripts/Framework/Bootstrap/bootstrap.js"));
 
             //Self defined JS
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
